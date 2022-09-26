70de0c7b1e711e96880292a359f4fba8cd3e4214
kwlucas <lucaswkyle@gmail.com>
2022-09-26 11:02:35 -0600

    Added get /notes request

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const path = require('path');
 
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
 
 
