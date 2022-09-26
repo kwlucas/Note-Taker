2de20c65de6796766b535f3715da106fa95ea131
kwlucas <lucaswkyle@gmail.com>
2022-09-26 12:48:44 -0600

    Created get api/notes route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
+const fs = require('fs').promises;
 
-
+router.get('/api/notes', async (req, res) => {
+    const dbContents = await fs.readFile('../db/db.json', 'utf-8');
+    let notes = JSON.parse(dbContents);
+    if (notes){
+        res.send(notes);
+    }
+    else{
+        res.status(404).send('Notes not found');
+    }
+});
 
 module.exports = router;
